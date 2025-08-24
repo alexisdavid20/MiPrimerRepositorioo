b454b797c0337beeeb29c470b58d411af88d872c
Alexis Aleman <alexisss.15.2005@gmail.com>
2025-08-24 11:33:40 -0600

    mi segunda paja

--- probando visual con java.java
+++ probando visual con java.java
@@ -1,5 +1,5 @@
 public class probando visual con java {
     public static void main(String[] args) {
-        System.out.println("Hola MundoooOOooOOoOoooOooo!");
+        System.out.println("adiooooss MundoooOOooOOoOoooOooo!");
     }
 }
